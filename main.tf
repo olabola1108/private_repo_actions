a0d3df15ff9aacf5191c1311f69d6ae32c82e05c
olabola1108 <olabola1108@gmail.com>
2023-12-10 17:33:26 -0500

    chore: First Commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+module "s3-backend" {
+  source = "git@github.com:olabola1108/Semantic-Versioning.git//s3-module?ref=v1.2.0"
+}
+
+ terraform {
+  backend "s3" {
+    bucket         = "new-dev-477"
+    key            = "actions/terraform.tfstate"
+    #dynamodb_table = "HR-terraform-remote-state-dynamo"
+    region         = "us-east-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ }
+ 
+provider "aws" {
+  region = us-east-2
+}
+ 
+
+
+
